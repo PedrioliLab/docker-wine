aa309c416b4ae47e38501b694679b17c2bc40d07
ppatrick <pedrioli@gmail.com>
2016-10-05 13:26:30 +0200

    Make host user match container user

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker run -e LOCAL_USER_ID -e DISPLAY -v $HOME/.Xauthority:/home/user/.Xauthority -it --rm --net=host ppatrick/wine $@
+docker run -e LOCAL_USER_ID=`id -u $USER` -e DISPLAY -v $HOME/.Xauthority:/home/user/.Xauthority -it --rm --net=host ppatrick/wine $@
